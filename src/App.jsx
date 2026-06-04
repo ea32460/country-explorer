69b37e11bd79caf8eee130e756952b71756083bc
ea32460 <ea32460@seeu.edu.mk>
2026-06-04 14:32:55 +0200

    Added favorites with localStorage and theme context

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,40 @@ import Countries from "./pages/Countries";
 import Favorites from "./pages/Favorites";
 import CountryDetails from "./pages/CountryDetails";
 import Navbar from "./components/Navbar.jsx";
+import {useEffect, useState} from "react";
 
 
 export default function App(){
+  const [favorites, setFavorites]= useState(()=>{
+    const saved = localStorage.getItem("favorites");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(()=>{
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const toggleFavorite =(country)=> {
+    setFavorites((prev)=>{
+      const exists=prev.find((c)=> c.cca3 ===country.cca3);
+
+      if (exists) {
+        return prev.filter((c) => c.cca3 !== country.cca3);
+      }else{
+        return [...prev,country];
+      }
+    });
+  };
+
   return (
-      <>
+      <div style={{
+        backgroundColor: darkMode ? "#121212" : "#f5f5f5",
+        color: darkMode ? "white" : "black",
+        minHeight: "100vh",
+        transition: "0,3s",
+      }}
+
+      >
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,6 +46,6 @@ export default function App(){
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/country/:name" element={<CountryDetails />} />
       </Routes>
-</>
-  );
-}
+      </div>
+      );
+  }
